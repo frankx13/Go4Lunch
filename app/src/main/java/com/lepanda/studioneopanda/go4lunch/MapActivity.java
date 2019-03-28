ab4a1ca1baa589437d879059958627f118b5587c
frankx13 <frankx13580@gmail.com>
2019-03-28 16:05:41 +0100

    Adding Navigation Drawer to MapActivity - Success

--- app/src/main/java/com/lepanda/studioneopanda/go4lunch/MapActivity.java
+++ app/src/main/java/com/lepanda/studioneopanda/go4lunch/MapActivity.java
@@ -5,25 +5,74 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        Menu menu = navigation.getMenu();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            //menu.findItem(R.id.navigation_map).setIcon(getDrawable(R.drawable.ic_map_black_24dp));
-            //menu.findItem(R.id.navigation_list).setIcon(getDrawable(R.drawable.ic_list_black_24dp));
-            //menu.findItem(R.id.navigation_workmates).setIcon(getDrawable(R.drawable.ic_people_black_24dp));
-        }
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
+                // set item as selected to persist highlight
+                menuItem.setChecked(true);
+                // close drawer when item is tapped
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()) {
+
+                    case R.id.my_lunch:
+                        break;
+
+                    case R.id.settings:
+                        break;
+
+                    case R.id.logout:
+                        AuthUI.getInstance()
+                                .signOut(MapActivity.this)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        // ...
+                                        Intent intent = new Intent(MapActivity.this, MainActivity.class);
+                                        Toast.makeText(MapActivity.this, "Logging out...", Toast.LENGTH_SHORT).show();
+                                        finish();
+                                        startActivity(intent);
+                                    }
+                                });
+                        break;
+
+                }
+
+
+                return true;
+            }
+        });
+
+        //Menu menu = navigation.getMenu();
+        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        //menu.findItem(R.id.navigation_map).setIcon(getDrawable(R.drawable.ic_map_black_24dp));
+        //menu.findItem(R.id.navigation_list).setIcon(getDrawable(R.drawable.ic_list_black_24dp));
+        //menu.findItem(R.id.navigation_workmates).setIcon(getDrawable(R.drawable.ic_people_black_24dp));
+        //}
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -36,7 +85,7 @@ public class MapActivity extends AppCompatActivity {
                         //item.setIcon(R.drawable.ic_list_black_24dp);
                         break;
                     case R.id.navigation_workmates:
-                        Intent b = new Intent(MapActivity.this,WorkmatesActivity.class);
+                        Intent b = new Intent(MapActivity.this, WorkmatesActivity.class);
                         startActivity(b);
                         //item.setIcon(R.drawable.ic_people_black_24dp);
                         break;
