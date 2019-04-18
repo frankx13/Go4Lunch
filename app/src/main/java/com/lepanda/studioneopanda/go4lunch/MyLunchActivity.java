206bc55295f97f3d067e73a93ed88b16ab20a9c9
frankx13 <frankx13580@gmail.com>
2019-04-18 23:01:31 +0200

    Terminated MyLunch functionnality

--- app/src/main/java/com/lepanda/studioneopanda/go4lunch/MyLunchActivity.java
+++ app/src/main/java/com/lepanda/studioneopanda/go4lunch/MyLunchActivity.java
@@ -1,7 +1,10 @@
 package com.lepanda.studioneopanda.go4lunch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyLunchActivity extends AppCompatActivity {
 
@@ -9,7 +12,20 @@ public class MyLunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_lunch);
+        returnToCentral();
     }
 
+    //STACKBACK
+    private void returnToCentral() {
+        Button btnBack = findViewById(R.id.btn_lunch_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyLunchActivity.this, CentralActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 
 }
